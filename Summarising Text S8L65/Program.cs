c596d614d6afee984b334fc50707b32ac96eefdd
Sartius <amer_hrnjic@hotmail.com>
2019-06-09 14:23:23 +0200

    Import Project

---
+++ Summarising Text S8L65/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summarising_Text_S8L65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a realy realy realy realy reay wajdawj woidjoaw oiajwd long text";
+            var summary = StringUtility.SummerizeText(sentence,25);
+            Console.WriteLine(summary);
+                    
+        }
+
+    }
+}
---
+++ Summarising Text S8L65/StringUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summarising_Text_S8L65
+{
+    public class StringUtility
+    {
+        public static string SummerizeText(string text, int maxlength = 20)
+        {
+            if (text.Length < maxlength)
+                return text;
+            else
+            {
+                // sentence.Substring(0, maxlength) bad implement
+                var words = text.Split(' ');
+                var totalCharacters = 0;
+                var summarywords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    summarywords.Add(word);
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > maxlength)
+                        break;
+                }
+
+                return String.Join(" ", summarywords) + "...";
+
+            }
+        }
+    }
+}
